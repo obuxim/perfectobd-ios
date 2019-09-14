713d110c0d40fc1ccddeb080a1cd6f3a3af7c56b
Zubair Hasan <zubo@octoriz.com>
2019-09-14 19:55:49 +0600

    Initial Commit

---
+++ Unikaservice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Unikaservice
+//
+//  Created by Zubair Hasan on 14/9/19.
+//  Copyright © 2019 Octoriz Private Limited. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
